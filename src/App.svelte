cb0e164ad44ca32bd0594825e35408412aa90443
SotaKawamoto <sg22786g@st.omu.ac.jp>
2023-10-10 12:33:25 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,40 @@
+<script>
+   
+
+    import pokeDetails, { pokemon } from './pokedetails.svelte'
+    let name;
+    
+    const promise = getPokemons();
+    let selectedPokemon = null;
+
+    async function getPokemons(){//非同期情報を得る関数の定義
+        const res =await fetch('https://pokeapi.co/api/v2/pokemon')//name,urlの入ったデータを得ることができる？指定までか？
+        const json =await res.json();//ここでjsonデータの入手か
+
+        return json.results;// ここにresult:name,urlが入ってる
+    }
+
+    const selectPokemon= ({detail}) => {//アローはっていうのはみたいな感じ
+        selectedPokemon = detail;
+    }
+</script>
+
+<h1>
+    Pokesearch
+</h1>
+
+<div>
+    {#await promise}<!--非同期（この時name,urlのjson）データの確認-->
+        searching pokemon
+    {:then pokemons} <!--pokelist先のデータ？？pokemons??url先のjosnデータ関係の気がするけど-->
+        <Pokelist pokemons={pokemons}
+
+
+</div>
+
+<main>
+    <input bind:value = {name} />
+    <h1>
+        output 
+    </h1>
+</main>
---
+++ src/pokedetails.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let pokemon//pokedetailsを使うときに引っ張てくる値をpokemon(これは任意？持ってくるデータで決まっている？) =に入れる。このときは、url?? {jsonデータを指すのか？}.はオブジェクトだろうけど.あと画像の切り替えもできてないけど、defaultからofficial artworkとかの切り替えできない漢字なん？
+    let promise
+    $:promise =getpoke(pokemon.url)
+
+    async function getpoke(url){
+        const res = await fetch(pokemon.url);
+        return await res.json();
+    }
+
+</script>
+
+<div>
+    {#await promise}
+        searching {pokemon.name}
+    {:then pokemonDetails}
+        <h2>{pokemonDetails.name}</h2>
+        <img src = {pokemonDetails.sprites.front_default} alt={pokemonDetails.name} />
+    {:catch error}
+        Error
+    {/await}
+<div/>
